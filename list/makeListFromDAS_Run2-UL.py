f918f3ad35407cfaf06b40438b72cc7fe1211e3b
sixie <si.xie@cern.ch>
2022-10-30 16:41:59 +0100

    Data

--- list/makeListFromDAS_Run2-UL.py
+++ list/makeListFromDAS_Run2-UL.py
@@ -40,6 +40,17 @@ datasets = {
 # "SingleMuon_2016F":"/SingleMuon/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD",
 # "SingleMuon_2016G":"/SingleMuon/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD",
 # "SingleMuon_2016H":"/SingleMuon/Run2016H-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2016B-v1_HIPM":"/SingleElectron/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD",
+# "SingleElectron_2016B-v2_HIPM":"/SingleElectron/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD",
+# "SingleElectron_2016C_HIPM":"/SingleElectron/Run2016C-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD",
+# "SingleElectron_2016D_HIPM":"/SingleElectron/Run2016D-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD",
+# "SingleElectron_2016E_HIPM":"/SingleElectron/Run2016E-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD",
+# "SingleElectron_2016F_HIPM":"/SingleElectron/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD",
+# "SingleElectron_2016F":"/SingleElectron/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2016G":"/SingleElectron/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2016H":"/SingleElectron/Run2016H-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD",
+
+
 # ####################################################################################################
 # #2017 Datasets
 # ####################################################################################################   
@@ -60,6 +71,12 @@ datasets = {
 # "SingleMuon_2017F":"/SingleMuon/Run2017F-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
 # "SingleMuon_2017G":"/SingleMuon/Run2017G-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
 # "SingleMuon_2017H":"/SingleMuon/Run2017H-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2017B":"/SingleElectron/Run2017B-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2017C":"/SingleElectron/Run2017C-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2017D":"/SingleElectron/Run2017D-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2017E":"/SingleElectron/Run2017E-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
+# "SingleElectron_2017F":"/SingleElectron/Run2017F-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD",
+
 ####################################################################################################
 #2018 Datasets
 ####################################################################################################   
@@ -75,6 +92,10 @@ datasets = {
 #"SingleMuon_2018B":"/SingleMuon/Run2018B-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD",
 #"SingleMuon_2018C":"/SingleMuon/Run2018C-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD",
 #"SingleMuon_2018D":"/SingleMuon/Run2018D-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD",
+#"EGamma_2018A":"/EGamma/Run2018A-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD",
+#"EGamma_2018B":"/EGamma/Run2018B-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD",
+#"EGamma_2018C":"/EGamma/Run2018C-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD",
+#"EGamma_2018D":"/EGamma/Run2018D-UL2018_MiniAODv2_NanoAODv9-v3/NANOAOD",
 
 # "DiPhotonJetsBox1BJet_MGG-80toInf_13TeV-sherpa":"/DiPhotonJetsBox1BJet_MGG-80toInf_13TeV-sherpa/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM",
 # "DiPhotonJetsBox2BJets_MGG-80toInf_13TeV-sherpa":"/DiPhotonJetsBox2BJets_MGG-80toInf_13TeV-sherpa/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM",
