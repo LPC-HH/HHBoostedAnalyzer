706a32f47f4147bf0e9b1b6b81662f8a8ddc00f7
sixie <si.xie@cern.ch>
2024-10-18 05:54:19 +0200

    update

---
+++ list/makeList.sh
@@ -0,0 +1,65 @@
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2022C\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2022C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2022D\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2022D.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2022E\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2022E.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2022F\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2022F.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2022G\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2022G.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2023C_[01]v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2023C_v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2023C_[01]v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2023C_v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2023C_[01]v3\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2023C_v3.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2023C_[01]v4\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2023C_v4.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2023D_[01]v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2023D_v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2023D_[01]v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2023D_v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024B\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024B.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024C\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024D\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024D.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024E-v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024E-v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024E-v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024E-v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024F\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024F.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024G\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024G.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/EGListFiles.txt | grep "2024H\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > EGamma_2024H.list
+
+
+
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2022C\/" | grep "JetHT" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetHT2022C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2022C\/" | grep -v "JetHT" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2022C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2022D\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2022D.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2022E\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2022E.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2022F\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2022F.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2022G\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2022G.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2023C_[01]v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2023C_v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2023C_[01]v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2023C_v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2023C_[01]v3\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2023C_v3.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2023C_[01]v4\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2023C_v4.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2023D_[01]v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2023D_v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2023D_[01]v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2023D_v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024B\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024B.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024C\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024D\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024D.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024E-v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024E-v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024E-v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024E-v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024F\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024F.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024G\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024G.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/JetMETListFiles.txt | grep "2024H\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > JetMET2024H.list
+
+
+
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2022C\/" | grep SingleMuon | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > SingleMuon_2022C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2022C\/" | grep -v SingleMuon | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2022C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2022D\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2022D.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2022E\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2022E.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2022F\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2022F.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2022G\/" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2022G.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2023C_[01]v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2023C_v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2023C_[01]v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2023C_v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2023C_[01]v3\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2023C_v3.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2023C_[01]v4\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2023C_v4.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2023D_[01]v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2023D_v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2023D_[01]v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2023D_v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024B\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024B.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024C\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024C.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024D\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024D.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024E-v1\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024E-v1.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024E-v2\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024E-v2.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024F\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024F.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024G\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024G.list
+cat /afs/cern.ch/work/s/sixie/public/releases/run3/analysis/HH/CMSSW_14_0_7/src/MuonListFiles.txt | grep "2024H\/" | grep -v "log" | sed 's/\/eos\/uscms/root:\/\/cmsxrootd.fnal.gov\//' > Muon_2024H.list
