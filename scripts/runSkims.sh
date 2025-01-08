009f228100ade277c2e7345b7a65062ad7cae4bb
sixie <si.xie@cern.ch>
2025-01-08 20:36:39 +0100

    add skims

---
+++ scripts/runSkims.sh
@@ -0,0 +1,17 @@
+./SkimNtuple SkinInputList_2022.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/1LSkim/2022/ 1LSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.4 && fatJet1_msoftdrop > 50 && lep1Pt > 50 && lep2Pt < 30 && fatJet2_pt < 200 && fatJet2_msoftdrop < 50 && MET > 50"
+./SkimNtuple SkinInputList_2022EE.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/1LSkim/2022EE/ 1LSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.4 && fatJet1_msoftdrop > 50 && lep1Pt > 50 && lep2Pt < 30 && fatJet2_pt < 200 && fatJet2_msoftdrop < 50 && MET > 50"
+./SkimNtuple SkinInputList_2023.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/1LSkim/2023/ 1LSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.4 && fatJet1_msoftdrop > 50 && lep1Pt > 50 && lep2Pt < 30 && fatJet2_pt < 200 && fatJet2_msoftdrop < 50 && MET > 50"
+./SkimNtuple SkinInputList_2023BPix.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/1LSkim/2023BPix/ 1LSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.4 && fatJet1_msoftdrop > 50 && lep1Pt > 50 && lep2Pt < 30 && fatJet2_pt < 200 && fatJet2_msoftdrop < 50 && MET > 50"
+./SkimNtuple SkinInputList_2024.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/1LSkim/2024/ 1LSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.4 && fatJet1_msoftdrop > 50 && lep1Pt > 50 && lep2Pt < 30 && fatJet2_pt < 200 && fatJet2_msoftdrop < 50 && MET > 50"
+
+
+./SkimNtuple SkinInputList_2022.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/TriggerEffQCDSkim/2022/ TriggerEffQCDSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.5 && fatJet1_msoftdrop > 50 && fatJet2_pt > 160"
+./SkimNtuple SkinInputList_2022EE.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/TriggerEffQCDSkim/2022EE/ TriggerEffQCDSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.5 && fatJet1_msoftdrop > 50 && fatJet2_pt > 160"
+./SkimNtuple SkinInputList_2023.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/TriggerEffQCDSkim/2023/ TriggerEffQCDSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.5 && fatJet1_msoftdrop > 50 && fatJet2_pt > 160"
+./SkimNtuple SkinInputList_2023BPix.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/TriggerEffQCDSkim/2023BPix/ TriggerEffQCDSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.5 && fatJet1_msoftdrop > 50 && fatJet2_pt > 160"
+./SkimNtuple SkinInputList_2024.txt /eos/uscms/store/group/lpcdihiggsboost/sixie/analyzer/HHTo4BNtupler/ArmenVersion/nano/run3/combined/TriggerEffQCDSkim/2024/ TriggerEffQCDSkim "fatJet1_pt > 250 && abs(fatJet1_eta) < 2.5 && fatJet1_msoftdrop > 50 && fatJet2_pt > 160"
+
+
+
+
+
